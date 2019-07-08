a75d63b5e3b7ed3f1e3cbfc6691755bca481dd46
h81189 <hari8koneru@gmail.com>
2019-07-07 22:24:11 -0400

    + sensor binary to temperature function

---
+++ research_docs/TemperatureSensorCode.py
@@ -0,0 +1,7 @@
+
+def binarytotemp(binary, positive): # binary is a string, positive is a boolean
+    if positive:
+        return sum([2**i for i in range(12) if binary[11-i]=="1"])/16
+    else:
+        return -(sum([2**i for i in range(12) if binary[11-i]=="0"])+1)/16
+    
