e7ef2b3e43baf5f9dd12eb1b4df3efbc6faf8fb8
Arya Gharib <aryakhan81@icloud.com>
2019-10-05 15:59:26 -0700

    Update readlux file to create spreadsheet for us

--- Rpi_Code/LED_code/readlux.py
+++ Rpi_Code/LED_code/readlux.py
@@ -1,9 +1,27 @@
 import board
 import busio
 import adafruit_tsl2591
+import csv
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tsl2591.TSL2591(i2c)
 
-while True:
-    print(sensor.lux)
+with open('lux_values.csv', 'wb') as csvfile:
+filewriter = csv.writer(csvfile, delimiter=',',
+                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+filewriter.writerow(['LED', 'Distance', 'LuxValue'])
+
+for led in range(48)
+
+    luxValues = []
+    distance = int(input())
+
+    for i in range(1000):
+        luxValues.append(sensor.lux)
+        
+    averageLux = sum(luxValues) / len(luxValues)
+
+    with open('lux_values.csv', 'a') as csvfile:
+        csvfile.writerow([ledName, distance, averageLux])
+
+
